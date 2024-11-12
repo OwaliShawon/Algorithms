7a8e28e7ea5781cc72b990f05ce01c57b4da9cc8
owalishawon <owali.shawon@gmail.com>
2024-11-12 15:38:41 +0600

    [add] bubble, selection and insertion sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ArraySort sorter = new ArraySort();
+
+        System.out.print("Enter Array size: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Choose sorting algorithm :::: ");
+        System.out.println("1. Bubble Sort");
+        System.out.println("2. Selection Sort");
+        System.out.println("3. Insertion Sort");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                array = sorter.bubbleSort(array);
+                System.out.println("bubble sorted: " + Arrays.toString(array));
+                break;
+            case 2:
+                array = sorter.selectionSort(array);
+                System.out.println("selection sorted: " + Arrays.toString(array));
+                break;
+            case 3:
+                array = sorter.insertionSort(array);
+                System.out.println("insertion sorted: " + Arrays.toString(array));
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+
+        scanner.close();
     }
 }
